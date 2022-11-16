e7d08879c00f4fbe8c0f0d348ddb88515985f627
melo_ <menadiermelodie@gmail.com>
2022-11-16 11:26:53 +0100

    Etape 1 : Afficher la liste des présidents

--- Q2.php
+++ Q2.php
@@ -1,1 +1,27 @@
+<?php header('Content-type: text/xml; Encoding: utf-8');
+$doc = new DOMDocument();
+$doc->preserveWhiteSpace = false;
+$doc->validateOnParse  = true;
+$doc->load("tp.xml");
 
+$p = $doc->documentElement->lastChild->firstChild;
+
+//Génération du résultat
+echo "<?xml version='1.0' encoding='UTF-8' ?>\n";
+echo "<!DOCTYPE deplacement SYSTEM 'tp.dtd'>\n";
+echo "<liste-présidents>\n";
+
+
+
+while (($p instanceOf DOMELEMENT) && ($p->tagName == 'personne')) {
+    $personne = $p;
+    $f = $personne->lastChild;
+    $fonction = $f->getAttribute("type");
+    if ($fonction == "Président de la République") {
+        $nom = $personne->getAttribute("nom");
+        echo "<Président nom=\"" .$nom . "\">"."\n";
+        }
+    $p=$p->nextSibling;
+}
+
+echo "</liste-présidents>\n";?>
