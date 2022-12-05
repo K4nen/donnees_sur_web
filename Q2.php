4fd2b1b76d38586a02127a597656b3394da21960
K4nen <menadiermelodie@gmail.com>
2022-12-05 16:40:49 +0100

    début Q3 + Merge Q2

--- Q2.php
+++ Q2.php
@@ -7,13 +7,14 @@ $fileopen=(fopen($file,'a'));
 ftruncate($fileopen, 0);
 
 $doc = new DOMDocument();
-
+$doc->preserveWhiteSpace = false;
+$doc->validateOnParse  = true;
 $doc->load("tp.xml");
 
-$pa = $doc->documentElement->firstChild->firstChild;
-$p = $doc->documentElement->lastChild->firstChild;
 
-echo $pa;
+$p = $doc->documentElement->lastChild->firstChild;
+$pa = $doc->documentElement->firstChild->firstChild;
+
 //Génération du résultat
 fwrite($fileopen, "<?xml version='1.0' encoding='UTF-8' ?>\n");
 fwrite($fileopen, "<!DOCTYPE deplacement SYSTEM 'tp.dtd'>\n");
@@ -28,29 +29,16 @@ while (($p instanceOf DOMELEMENT) && ($p->tagName == 'personne')) {
     if ($fonction == "Président de la République") {
         $nom = $personne->getAttribute("nom");
         //Ecriture dans le fichier texte
-        fwrite($fileopen, "<president nom=\"" .$nom . "\">"."\n");
-        $nomP = "test";
-        $continent = "";
-
-        while (($pa instanceOf DOMELEMENT) && ($pa->tagName == 'pays')) {
-            $nomP = "oui";
-            $pays = $pa;
-            $c = $pays->firstChild;
-            $continent = $c->getAttribute("continent");
-            if($continent == 'africa'){
-                $nomP .=$pays->getAttribute('name')."\n";
-            }
-
-        }
-        fwrite($fileopen, $nomP."\n");
+        fwrite($fileopen, "<Président nom=\"" .$nom . "\">"."\n");
+        $res = affichepays();
+        fwrite($fileopen, $res."\n");
 
     }
     $p=$p->nextSibling;
 }
 function affichepays($pa){
-    $nom = "";
-    $continent = "";
     while (($pa instanceOf DOMELEMENT) && ($pa->tagName == 'pays')) {
+        $nom = "";
         $pays = $pa;
         $c = $pays->firstChild;
         $continent = $c->getAttribute("continent");
@@ -59,7 +47,7 @@ function affichepays($pa){
         }
 
     }
-return $continent;
+return $nom;
 
 }
 
--- Q3.php
+++ Q3.php
@@ -1,1 +1,74 @@
+<?php
+include('Sax4php.php');
+
+class ListePresidents extends DefaultHandler {
+    private $texte;
+
+    function __construct($a) {
+        parent::__construct(); $this->age = $a;
+    }
+
+    function startDocument() {
+        echo "<?xml version='1.0' encoding='UTF-8' ?>\n"; //<?php
+        echo "<!DOCTYPE liste-presidents SYSTEM 'liste-presidents.dtd'>\n";
+        echo "<liste-presidents>\n";
+    }
+
+    function endDocument() {
+        echo "</liste-presidents>\n";
+    }
+
+    function characters($txt) {
+        $txt = trim($txt);
+        if (!(empty($txt))) $this->texte .= $txt;
+    }
+
+    function startElement($nom, $att)
+    {
+        if (!empty($nom)) {
+            switch ($nom) {
+                case 'liste-pays' :
+                    $lpays = [];
+                case 'pays' :
+                    //var_dump($att);
+                    if (!empty($att["nomP"])) {
+                        $pa = ["nomP" => $att["nom"], "id" => $att["xml:id"], "francophone" => null];
+                        array_push($lpays, $pa);
+                        foreach ($lpays as $p) {
+                            var_dump($p);
+                            //echo $p["nomP"];
+                            //echo $p["nomP"]."</br>";
+                            /*
+                            echo $p["id"]."</br>";
+                            */
+                        }
+                    }
+                //echo $att["nom"]."</br>";
+                /*
+                case 'encompassed' :
+                    if ($att["continent"] == "africa")
+                    {
+                    }
+                    ;
+    */
+                default;
+
+            }
+        }
+    }
+
+    function endElement($nom) {
+        switch($nom) {
+            case 'titre' : if ($this->ok) echo " titre='$this->texte'/>\n"; break;
+            default :;
+        }
+    }
+}
+
+try {
+    $sax = new SaxParser( new ListePresidents(9) );
+    $sax->parse('tp.xml');
+} catch(SAXException $e){ echo "\n",$e;
+} catch(Exception $e) { echo "Capture de l'exception par défaut\n", $e;
+}?>
 
