f03feaa505253c2221a33577a4cc73f610376909
tomba <tom.barillet@hotmail.fr>
2022-12-05 17:50:06 +0100

    Affichage des présidents et des pays

--- Q2.php
+++ Q2.php
@@ -1,5 +1,5 @@
 <?php header('Content-type: text/xml; Encoding: utf-8');
-
+ini_set("memory_limit", -1);
 //Chemin vers le fichier texte
 $file = "finQ2.xml";
 //Ouverture en mode écriture
@@ -10,6 +10,10 @@ $doc = new DOMDocument();
 $doc->preserveWhiteSpace = false;
 $doc->validateOnParse  = true;
 $doc->load("tp.xml");
+$xpath = new DOMXPath($doc);
+foreach ($xpath->query('//comment()') as $comment) {
+    $comment->parentNode->removeChild($comment);
+}
 
 
 $p = $doc->documentElement->lastChild->firstChild;
@@ -20,7 +24,8 @@ fwrite($fileopen, "<?xml version='1.0' encoding='UTF-8' ?>\n");
 fwrite($fileopen, "<!DOCTYPE deplacement SYSTEM 'tp.dtd'>\n");
 fwrite($fileopen, "<liste-présidents>\n");
 
-
+$president = array();
+$listpays = array();
 
 while (($p instanceOf DOMELEMENT) && ($p->tagName == 'personne')) {
     $personne = $p;
@@ -29,28 +34,42 @@ while (($p instanceOf DOMELEMENT) && ($p->tagName == 'personne')) {
     if ($fonction == "Président de la République") {
         $nom = $personne->getAttribute("nom");
         //Ecriture dans le fichier texte
-        fwrite($fileopen, "<Président nom=\"" .$nom . "\">"."\n");
-        $res = affichepays();
-        fwrite($fileopen, $res."\n");
+        /*
 
+
+        $res = affichepays($pa);
+        foreach($res as $c){
+            fwrite($fileopen, $c."\n");
+        }
+
+        */
+        array_push($president,$nom);
     }
     $p=$p->nextSibling;
 }
-function affichepays($pa){
-    while (($pa instanceOf DOMELEMENT) && ($pa->tagName == 'pays')) {
-        $nom = "";
-        $pays = $pa;
-        $c = $pays->firstChild;
+while (($pa instanceOf DOMELEMENT) && ($pa->tagName == 'pays')) {
+    $nomP="";
+    $pays = $pa;
+    $c = $pays->firstChild;
+    if (!is_null($c)) {
         $continent = $c->getAttribute("continent");
-        if($continent == 'africa'){
-            $nom .=$pays->getAttribute('name')."\n";
-        }
+    }
+    if($continent == 'africa'){
+        $nomP .=$pays->getAttribute('nom');
+        array_push($listpays,$nomP);
+        var_dump($listpays);
 
     }
-return $nom;
+    $pa=$pa->nextSibling;
 
 }
-
+foreach($president as $pre){
+    fwrite($fileopen, "<president nom=\"" .$pre . "\">"."\n");
+    foreach($listpays as $lp){
+        fwrite($fileopen,"   <pays nom=\"".$lp."\" durée=\"0\"/>"."\n");
+    }
+    fwrite($fileopen, "</president>"."\n");
+}
 
 
 fwrite($fileopen, "</liste-présidents>\n");
