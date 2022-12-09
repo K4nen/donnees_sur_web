0817bdb219396677365cce58319927eebbe0ee8c
K4nen <menadiermelodie@gmail.com>
2022-12-09 10:49:37 +0100

    Sax qui fonctionne LET'S GO ENFAITE

--- Q3.php
+++ Q3.php
@@ -1,22 +1,71 @@
-<?php
+<?php header('Content-type: text/xml; Encoding: utf-8');
+
+function startDocument() {
+    echo"<?xml version='1.0' encoding='UTF-8' ?>\n";
+    echo "<!DOCTYPE liste-présidents SYSTEM 'tp.dtd'> \n";
+    echo "<liste-presidents>\n";
+}
+
+
+function endDocument() {
+    global $tabPays, $tabVisites, $tabPersonnes;
+    foreach($tabPersonnes as $unePersonne) {
+        echo "\t<président nom=\"".$unePersonne['NOMPERSONNE']."\">\n";
+        foreach($tabPays as $unPays) {
+            $duree = 0;
+            foreach($tabVisites as $uneVisite) {
+                if(($uneVisite['PAYS'] == $unPays['IDPAYS']) && ($uneVisite['PERSONNE'] == $unePersonne['IDPERSONNE'])) {
+                    if($uneVisite['DEBUT'] == $uneVisite['FIN']) {
+                        $duree += 1;
+                    }else{
+                        $debut = strtotime($uneVisite['DEBUT']);
+                        $fin = strtotime($uneVisite['FIN']);
+                        $diff = $fin - $debut;
+                        $duree += 1 + round($diff / 86400);
+                    }
+                }
+            }
+            if($unPays['FRANCOPHONE'] != null) {
+                $francophone = " francophone=\"".$unPays['FRANCOPHONE']."\"";
+            }else{
+                $francophone = "";
+            }
+            if($duree == 0) {
+                $temps = "duree=\"0\"";
+            }else{
+                $temps = "duree='P".$duree."D'";
+            }
+            echo "\t\t<pays nom=\"".$unPays['NOMPAYS']."\" $temps$francophone/>\n";
+        }
+        echo "\t</président>\n";
+    }
+    echo "</liste-presidents>";
+}
+
+function characters($parser, $txt) {
+    global $texte;
+    $txt = trim($txt);
+    if (!(empty($txt))) $texte = $txt;
+}
+
 function startElements($parser, $name, $attrs) {
-    global $listePays, $listeVisites, $elements, $pays, $nomPays, $isAfrica, $pourcentage,$isFrench, $isPresident, $listePersonnes, $personne;
+    global $tabPays, $tabVisites, $elements, $pays, $enAfrique, $pourcentage,$francophone, $tabPersonnes, $personne;
 
     if(!empty($name)) {
         if ($name == 'LISTE-PAYS') {
-            $listePays []= array();
+            $tabPays= array();
         }
         if($name == 'PAYS') {
-            $isFrench = false;
-            $isAfrica = false;
-            $pays = ['NOM' => $attrs['NOM'], 'ID' => $attrs['XML:ID'], 'FRANCOPHONE' => null];
+            $francophone = false;
+            $enAfrique = false;
+            $pays = ['NOMPAYS' => $attrs['NOM'], 'IDPAYS' => $attrs['XML:ID'], 'FRANCOPHONE' => null];
         }
         if($name == 'ENCOMPASSED') {
             if($attrs['CONTINENT'] == 'africa') {
-                $isAfrica = true;
+                $enAfrique = true;
             }
         }
-        if($name == 'LANGUAGE' && $isAfrica) {
+        if($name == 'LANGUAGE' && $enAfrique) {
             if(isset($attrs['PERCENTAGE'])) {
                 $pourcentage = $attrs['PERCENTAGE'];
             }else{
@@ -24,62 +73,50 @@ function startElements($parser, $name, $attrs) {
             }
         }
         if($name == 'LISTE-VISITES') {
-            $listeVisites []= array();
+            $tabVisites= array();
         }
         if($name == 'VISITE') {
-            if(in_array($attrs['PAYS'], array_column($listePays, 'NOM'))) {
-                array_push($listeVisites, ['DEBUT' => $attrs['DEBUT'], 'FIN' => $attrs['FIN'], 'PERSONNE' => $attrs['PERSONNE'], 'PAYS' => $attrs['PAYS']]);
+            if(in_array($attrs['PAYS'], array_column($tabPays, 'IDPAYS'))) {
+                array_push($tabVisites, ['DEBUT' => $attrs['DEBUT'], 'FIN' => $attrs['FIN'], 'PERSONNE' => $attrs['PERSONNE'], 'PAYS' => $attrs['PAYS']]);
             }
         }
         if($name == 'LISTE-PERSONNES') {
-            $listePersonnes []= array();
+            $tabPersonnes = array();
         }
         if($name == 'PERSONNE') {
-            $isPresident = false;
-            $personne = ['NOMP'=> $attrs['NOM'], 'IDP' => $attrs['XML:ID']];
+            $personne = ['NOMPERSONNE'=> $attrs['NOM'], 'IDPERSONNE' => NULL];
 
         }
         if($name == 'FONCTION') {
+            $personne['IDPERSONNE'] = $attrs['XML:ID'];
             if($attrs['TYPE'] == 'Président de la République') {
-                $isPresident = true;
+                array_push($tabPersonnes, $personne);
             }
         }
-
         $elements = $name;
     }
 }
 
-function characterData($parser, $data) {
-    global $listePays, $listeVisites, $elements, $pays, $isAfrica, $pays, $pourcentage, $isFrench, $isPresident, $listePersonnes, $personne;
-
-    if(!empty($data)) {
-        if ($elements == 'LANGUAGE' && $isAfrica) {
-            if($data == 'French') {
-                $isFrench = true;
-                if($pourcentage == null) {
-                    $pays['FRANCOPHONE'] = "Officiel";
-                }else if($pourcentage > 30) {
-                    $pays['FRANCOPHONE'] = "En partie";
-                }
-                array_push($listePays, $pays);
-            }
-        }
-    }
-}
-
-
 function endElements($parser, $name) {
-    global $listePays, $elements, $pays, $isAfrica, $pays, $pourcentage, $isFrench, $isPresident, $listePersonnes, $personne;
+    global $texte, $tabPays, $enAfrique, $pays, $pourcentage, $francophone;
 
     if(!empty($name)) {
         if($name == 'PAYS') {
-            if(!$isFrench && $isAfrica) {
-                array_push($listePays, $pays);
+            if(!$francophone && $enAfrique) {
+                array_push($tabPays, $pays);
             }
         }
-        if($name == 'FONCTION') {
-            if($isPresident) {
-                array_push($listePersonnes,$personne);
+        if($name == 'LANGUAGE') {
+            if ($enAfrique) {
+                if($texte == 'French') {
+                    $francophone = true;
+                    if($pourcentage == null) {
+                        $pays['FRANCOPHONE'] = "Officiel";
+                    }else if($pourcentage > 30) {
+                        $pays['FRANCOPHONE'] = "En partie";
+                    }
+                    array_push($tabPays, $pays);
+                }
             }
         }
     }
@@ -89,34 +126,16 @@ function endElements($parser, $name) {
 $parser = xml_parser_create();
 
 xml_set_element_handler($parser, "startElements", "endElements");
-xml_set_character_data_handler($parser, "characterData");
+xml_set_character_data_handler($parser, "characters");
 
 if (!($handle = fopen('tp.xml', "r"))) {
     die("could not open XML input");
 }
-
+startDocument();
 while($data = fread($handle, 4096)) {
     xml_parse($parser, $data);
 }
-
-foreach($listePays as $tPays) {
-    //var_dump($tPays);
-    echo $tPays['NOM'];
-    echo "<br/>";
-}
-foreach($listeVisites as $tVisite) {
-    //var_dump($tVisite);
-    echo $tVisite['PERSONNE'];
-    echo $tVisite['PAYS'];
-    echo "<br/>";
-}
-
-foreach($listePersonnes as $tPersonne) {
-    var_dump($tPersonne);
-    echo "<br/>";
-}
-
+endDocument();
 xml_parser_free($parser); // deletes the parser
-
 
 ?>
