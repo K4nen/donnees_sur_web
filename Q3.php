cb88cf37639357fc72f389bd4535dc2b2c1455cc
K4nen <menadiermelodie@gmail.com>
2022-12-16 10:29:36 +0100

    SAX qui est push dans un fichier xml, et y'a des commentaires mais à revoir parce que je pense j'ai fais de la d

--- Q3.php
+++ Q3.php
@@ -1,20 +1,34 @@
 <?php header('Content-type: text/xml; Encoding: utf-8');
 
-function startDocument() {
-    echo"<?xml version='1.0' encoding='UTF-8' ?>\n";
-    echo "<!DOCTYPE liste-présidents SYSTEM 'tp.dtd'> \n";
-    echo "<liste-presidents>\n";
+//Chemin vers le fichier texte
+$file = "finQ3.xml";
+//Ouverture en mode écriture
+$fileopen=(fopen($file,'a'));
+ftruncate($fileopen, 0);
+
+
+//En-tête pour le document XLM, ouverture de la première balise "liste-présidents"
+function startDocument($fileopen) {
+    fwrite($fileopen,"<?xml version='1.0' encoding='UTF-8' ?>\n");
+    fwrite($fileopen, "<!DOCTYPE liste-présidents SYSTEM 'tp.dtd'> \n");
+    fwrite($fileopen, "<liste-presidents>\n");
 }
 
 
-function endDocument() {
+function endDocument($fileopen) {
+    //Variables pour parcourir les tableaux
     global $tabPays, $tabVisites, $tabPersonnes;
+    //On parcours le tableau des personnes pour afficher le nom
     foreach($tabPersonnes as $unePersonne) {
-        echo "\t<président nom=\"".$unePersonne['NOMPERSONNE']."\">\n";
+        fwrite($fileopen, "\t<président nom=\"".$unePersonne['NOMPERSONNE']."\">\n");
+        //On parcours ensuite les pays
         foreach($tabPays as $unPays) {
-            $duree = 0;
+            $duree = 0; //durée est initalisé à 0 car s'il n'y à pas de visite, durée = 0
+            //On parcours ensuite les visites pour récupérer les pays visité par la personne scannée
             foreach($tabVisites as $uneVisite) {
+                //On fait le lien dans le if entre un pays et une visite et une personne et une visite
                 if(($uneVisite['PAYS'] == $unPays['IDPAYS']) && ($uneVisite['PERSONNE'] == $unePersonne['IDPERSONNE'])) {
+                    //On calcul la durée de la visite, sachant que si la personne est aller 2 fois dans le même pays, on additionne la durée de la visite
                     if($uneVisite['DEBUT'] == $uneVisite['FIN']) {
                         $duree += 1;
                     }else{
@@ -25,23 +39,28 @@ function endDocument() {
                     }
                 }
             }
+            //Permet l'affichage de la balise francophone, officiel si oui, vide si non francophone
             if($unPays['FRANCOPHONE'] != null) {
                 $francophone = " francophone=\"".$unPays['FRANCOPHONE']."\"";
             }else{
                 $francophone = "";
             }
+            //affichage de la balise durée avec la durée entre le "P" et le "D"
             if($duree == 0) {
                 $temps = "duree=\"0\"";
             }else{
                 $temps = "duree='P".$duree."D'";
             }
-            echo "\t\t<pays nom=\"".$unPays['NOMPAYS']."\" $temps$francophone/>\n";
+            //affichage de la balise nom du pays
+            fwrite($fileopen,"\t\t<pays nom=\"".$unPays['NOMPAYS']."\" $temps$francophone/>\n");
         }
-        echo "\t</président>\n";
+        //fermeture de balise xml "présidents"
+        fwrite($fileopen,"\t</président>\n");
     }
-    echo "</liste-presidents>";
+    //fermeture de la balise xml "liste-présidents"
+    fwrite($fileopen,"</liste-presidents>");
 }
-
+//fonction qui permet de gérer et lire les noeuds texte
 function characters($parser, $txt) {
     global $texte;
     $txt = trim($txt);
@@ -52,19 +71,28 @@ function startElements($parser, $name, $attrs) {
     global $tabPays, $tabVisites, $elements, $pays, $enAfrique, $pourcentage,$francophone, $tabPersonnes, $personne;
 
     if(!empty($name)) {
+        //Quand on rencontre le noeud 'LISTE-PAYS' on crée un tableau vide "tabPays" pour stocker les pays
         if ($name == 'LISTE-PAYS') {
             $tabPays= array();
         }
+        /*
+         * Quand on rencontre le noeud 'PAYS', on crée 2 variables "francophone" et "enAfrique" qu'on met à false pour l'initialisation
+         * $francophone = vérifie si un pays est francophone selon les règles énoncées dans l'énoncé
+         * $enAfrique = vérifie qu'un pays est dans le continent "Africa"
+         * $pays = tableau qui stocke les informations pour un pays
+         */
         if($name == 'PAYS') {
             $francophone = false;
             $enAfrique = false;
             $pays = ['NOMPAYS' => $attrs['NOM'], 'IDPAYS' => $attrs['XML:ID'], 'FRANCOPHONE' => null];
         }
+        //Traitement de la variable $enAfrique
         if($name == 'ENCOMPASSED') {
             if($attrs['CONTINENT'] == 'africa') {
                 $enAfrique = true;
             }
         }
+        //Traitement pour la variable $francophone
         if($name == 'LANGUAGE' && $enAfrique) {
             if(isset($attrs['PERCENTAGE'])) {
                 $pourcentage = $attrs['PERCENTAGE'];
@@ -72,21 +100,34 @@ function startElements($parser, $name, $attrs) {
                 $pourcentage = null;
             }
         }
+        //Quand on rencontre le noeud 'LISTE-VISITES' on crée un tableau vide "tabVisites" pour stocker les visites
         if($name == 'LISTE-VISITES') {
             $tabVisites= array();
         }
+        /*
+         * Quand on rencontre le noeud 'VISITE', on va créer un tableau qui contient les informations pour chaque visites
+         * Et on va mettre cette visite dans le tableau "tabVisites"
+         */
         if($name == 'VISITE') {
             if(in_array($attrs['PAYS'], array_column($tabPays, 'IDPAYS'))) {
                 array_push($tabVisites, ['DEBUT' => $attrs['DEBUT'], 'FIN' => $attrs['FIN'], 'PERSONNE' => $attrs['PERSONNE'], 'PAYS' => $attrs['PAYS']]);
             }
         }
+        //Quand on rencontre le noeud 'LISTE-ERSONNES' on crée un tableau vide "tabPersonnes" pour stocker les personnes
         if($name == 'LISTE-PERSONNES') {
             $tabPersonnes = array();
         }
+        /*
+         * Quand on rencontre le noeud 'VISITE', on va créer un tableau qui contient les informations pour chaque visite
+         * On met l'IDPERSONNE à null pour le moment car il va être rempli par l'id rencontré dans la balise fonction juste en-dessous
+         */
         if($name == 'PERSONNE') {
             $personne = ['NOMPERSONNE'=> $attrs['NOM'], 'IDPERSONNE' => NULL];
 
         }
+        /*
+         * 
+         */
         if($name == 'FONCTION') {
             $personne['IDPERSONNE'] = $attrs['XML:ID'];
             if($attrs['TYPE'] == 'Président de la République') {
@@ -131,11 +172,11 @@ xml_set_character_data_handler($parser, "characters");
 if (!($handle = fopen('tp.xml', "r"))) {
     die("could not open XML input");
 }
-startDocument();
+startDocument($fileopen);
 while($data = fread($handle, 4096)) {
     xml_parse($parser, $data);
 }
-endDocument();
+endDocument($fileopen);
 xml_parser_free($parser); // deletes the parser
-
+fclose($fileopen);
 ?>
